20f8715b4aad9fd4f5ce3996b648ad5222f3cbe1
jgalilee <jack.galilee@trineo.com.au>
2012-10-13 13:32:47 +1100

    Add pending specs

--- spec/sObjectClassSpec.js
+++ spec/sObjectClassSpec.js
@@ -8,55 +8,203 @@
 describe('sObjectClass.js', function() {
 
   beforeEach(function() {
-    
-  });
-
-  describe('._getGeneralUrl()', function() {
-    
-  });
-
-  describe('._getSpecificUrlFor()', function() {
-    
+    subject = new sObjectClass(stubs.sObjects, stubs.schemaStub);
   });
 
   describe('._deferUnlessLoaded()', function() {
+
+    describe('not loaded', function() {
+
+      it('calls the load function if class is not loaded', function() {
+
+      });
+
+      it('calls the defered function after the load function', function() {
+        
+      });
+
+    })
+
+    describe('not loaded', function() {
+
+      it('does not call the load function if the class is loaded', function() {
+
+      });
+
+      it('calls the defered function after the load function', function() {
+        
+      });
+
+    });
+
+  });
+
+  describe('.url()', function() {
     
+    it('returns the url as specified by the schema object', function() {
+      expect(subject.url()).toEqual("http://sobject.class/");
+    }); 
+
   });
 
   describe('.is()', function() {
+
+    it('returns value for provided key in the private is object', function() {
+
+    });
     
   });
 
   describe('.load()', function() {
-    
+
+    it('loads the fields for the sobject schema', function() {
+
+    });
+
+    it('marks the class as loaded', function() {
+
+    });
+
+    it('calls the ready function after loading the schema', function() {
+
+    });
+
+    it('returns itself', function() {
+
+    });
+
   });
 
   describe('.className()', function() {
     
+    it('returns the name of the provided schema object', function() {
+
+    });
+
   });
 
   describe('.find()', function() {
     
+    it('returns a SOQL builder', function() {
+
+    });
+
+    it('returns a SOQL builder with a SELECT added', function() {
+
+    });
+
+    it('returns a SOQL builder with a SELECT and FROM added', function() {
+
+    });
+
   });
 
   describe('.build()', function() {
     
+    describe('one set of attributes', function() {
+
+      it('returns a new record', function() {
+
+      });
+
+      it('returns a new record with the provided attributes', function() {
+
+      });
+
+    });
+
+    describe('multiple sets of attributes', function() {
+
+      it('returns an array of the new records', function() {
+
+      });
+
+      it('creates a new record for each of the attribute sets', function() {
+
+      });
+
+    });
+
   });
 
   describe('.create()', function() {
-    
+
+    it('makes a request with the POST verb', function() {
+
+    });
+
+    it('makes a request to the class url', function() {
+
+    });
+
+    it('calls the after function with the data object', function() {
+
+    });
+
+    it('returns the provided record', function() {
+
+    });
+
   });
 
   describe('.read()', function() {
-    
+
+    it('makes a request with the GET verb', function() {
+
+    });
+
+    it('makes a request to the records url', function() {
+
+    });
+
+    it('calls the after function with the data object', function() {
+
+    });
+
+    it('returns the provided record', function() {
+
+    });
+
   });
 
   describe('.update()', function() {
-    
+
+    it('makes a request with the PATCH verb', function() {
+
+    });
+
+    it('makes a request to the records url', function() {
+
+    });
+
+    it('calls the after function with the data object', function() {
+
+    });
+
+    it('returns the provided record', function() {
+
+    });
+
   });
 
   describe('.delete()', function() {
-    
+
+    it('makes a request with the DELETE verb', function() {
+
+    });
+
+    it('makes a request to the records url', function() {
+
+    });
+
+    it('calls the after function with the data object', function() {
+
+    });
+
+    it('returns the provided record', function() {
+
+    });
+
   });
 
 });
--- spec/sObjectsSpec.js
+++ spec/sObjectsSpec.js
@@ -8,22 +8,62 @@
 describe('sObjects.js', function() {
 
   beforeEach(function() {
-    subject = new sObjects();
+
   });
 
   describe('.ajax()', function() {
 
-  });
+    it('performs a request with the oauth token', function() {
 
-  describe('.loadUrls()', function() {
+    });
+
+    it('performs a request with the provided verb', function() {
+
+    });
+
+    it('performs a request with the JSON content type', function() {
+
+    });
+
+    it('performs a request with the provided sucess function', function() {
+
+    });
+
+    describe('data provided in options', function() {
+
+      it('performs a request with body set to provided data value', function() {
+
+      });
+
+    });
+
+    describe('data not provided in options', function() {
+
+      it('performs a request without a body', function() {
+
+      });
+
+    });
 
   });
 
   describe('.loadObjects()', function() {
 
-  });
+    it('requests all of the sObject schemas from the org instance', function() {
 
-  describe('.loadObjects()', function() {
+    });
+
+    it('creates a new sObject class for each schema', function() {
+
+    });
+
+    it('assigns each new sObjectClass as a public attribute', function() {
+
+    });
+
+    it('calls the ready function after loading the schemas', function() {
+
+    });
 
   });
 
